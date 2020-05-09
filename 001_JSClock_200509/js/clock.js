31dde80cd619355eaef4fa8ada783a5d86c08fe0
madhandz5 <madhandz5@naver.com>
2020-05-10 03:56:48 +0900

    Final

--- 001_JSClock_200509/js/clock.js
+++ 001_JSClock_200509/js/clock.js
@@ -4,9 +4,10 @@ function getTime() {
     const now = new Date();
     const hours = now.getHours();
     const minutes = now.getMinutes();
-    const time = `${hours < 10 ? `0${hours}` : hours}:${
-    minutes < 10 ? `0${minutes}` : minutes
-  }`;
+    const seconds = now.getSeconds();
+    const time = `${hours < 10 ? `0${hours}` : hours}:
+    ${minutes < 10 ? `0${minutes}` : minutes}:
+    ${seconds < 10 ? `0${seconds}` : seconds}`;
     clock.innerHTML = time;
     return;
 }
